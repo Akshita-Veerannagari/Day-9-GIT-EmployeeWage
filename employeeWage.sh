4cabf235190af72431557809547fc33721ded621
Akshita-Veerannagari <akshitareddy.v@gmail.com>
2020-07-13 20:55:39 +0530

    Added condition to calculate wage until total working hrs or days is reached for a month

--- employeeWage.sh
+++ employeeWage.sh
@@ -3,19 +3,23 @@
 echo "Welcome to employee wage computation"
 isPartTime=1
 isFullTime=2
-totalSalary=20
+MAX_HRS_IN_MONTH=100
 empRatePerHr=20
 numWorkingDays=20
-for (( day=1; day<=$numWorkingDays; day++ ))
+
+totalEmpHrs=0
+totalWorkingDays=0;
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $numWorkingDays ]]
 do
+        (( totalWorkingDays++ ))
         empCheck=$(( RANDOM%3 ))
         case $empCheck in
                 $isFullTime) empHrs=8 ;;
                 $isPartTime) empHrs=4 ;;
                 *) empHrs=0 ;;
         esac
-
-        salary=$(( $empHrs*$empRatePerHr ));
-        totalSalary=$(( $totalSalary+$salary ))
+        totalEmpHrs=$(( $totalEmpHrs+$empHrs ))
 done
+totalSalary=$(( $totalEmpHrs*$empRatePerHr ))
 echo "Salary of all employees in a month is $totalSalary"
